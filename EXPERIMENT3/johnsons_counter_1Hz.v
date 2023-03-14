0288d440f1c1feadda03f79289df314cdb21fd53
Siddharth Kolte <127622281+SiddharthXXIII@users.noreply.github.com>
2023-03-14 10:47:57 +0530

    Update johnsons_counter_1Hz.v
    Clock divider instantiation was missing

--- EXPERIMENT3/johnsons_counter_1Hz.v
+++ EXPERIMENT3/johnsons_counter_1Hz.v
@@ -18,20 +18,20 @@
 // Additional Comments:
 // 
 //////////////////////////////////////////////////////////////////////////////////
-module Johnsons_counter_1_Hz();
-
-endmodule
-
-module johnson4bit(q3,q2,q1,q0,clk,rst);
-    input clk, rst;
+module johnson4bit(q3,q2,q1,q0,clk_50,rst);
+    input clk_50, rst;
     output q0,q1,q2,q3;
     
     wire q3bar, q2bar, q1bar, q0bar;
+    wire clk;
+	 
+	clock_divider C1(.clk_50(clk_50), .clk(clk));
     
-    dflipflop_withreset D3(q3,q3bar,q0bar,rst,clk);
+	dflipflop_withreset D3(q3,q3bar,q0bar,rst,clk);
     dflipflop_withreset D2(q2,q2bar,q3,rst,clk);
     dflipflop_withreset D1(q1,q1bar,q2,rst,clk);
     dflipflop_withreset D0(q0,q0bar,q1,rst,clk);
+	 
 endmodule
 
 
