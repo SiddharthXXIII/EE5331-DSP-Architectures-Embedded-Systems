d1dca4835f288aa2978f9aa0463c5743e147b8b7
Siddharth Kolte <127622281+SiddharthXXIII@users.noreply.github.com>
2023-03-28 15:42:07 +0530

    Update cordic_sequential_tb.v

--- EXPERIMENT4/cordic_sequential_tb.v
+++ EXPERIMENT4/cordic_sequential_tb.v
@@ -38,7 +38,9 @@ module cordic_tb();
             clk = 1'b0;
             
             #1 x_in = 8'd8; y_in = 8'd6;
-            #17 x_in = 8'd6; y_in = 8'd8; 
+            #50 x_in = -8'd8; y_in = 8'd6;
+            #50 x_in = -8'd8; y_in = -8'd6;
+            #50 x_in = 8'd8; y_in = -8'd6; 
         end
         
     initial
@@ -47,4 +49,3 @@ module cordic_tb();
         end
      always #1 clk = ~clk;
 endmodule
-
